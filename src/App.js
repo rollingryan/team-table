df5318601379209e3e336b172f8f8905cf9b4c1f
Ryan Donald <38063522+rollingryan@users.noreply.github.com>
2021-10-09 11:51:01 +0100

    Feat/add team (#3)
    * Include bootstra
    
    * Add team form functionality
    
    * update codeowners

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import React from 'react'
-import logo from './logo.svg'
-import { Counter } from './features/counter/Counter'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import AddTeam from './components/AddTeam'
+import Report from './components/Report'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className='App-link'
-            href='https://reactjs.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux-toolkit.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className='App-link'
-            href='https://react-redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  )
-}
+const App = () => (
+  <div className='wrapper'>
+    <Container fluid>
+      <Header />
+      <AddTeam />
+      <Report />
+    </Container>
+  </div>
+)
 
 export default App
